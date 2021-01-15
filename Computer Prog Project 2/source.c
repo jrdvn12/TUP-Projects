40d2148f857e5b035ba068e57865eb558b1b9fa7
jovan bruno <jaredivan.bruno@tup.edu.ph>
2021-01-15 10:35:32 +0800

    Update source.c

--- Computer Prog Project 2/source.c
+++ Computer Prog Project 2/source.c
@@ -1,33 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
-void settings() {
-	printf("Settings screen will be shown.\n");
-	return;
+void addStudent() {
+	system("cls");
+	printf("Add Student Screen\n");
+	printf("Press ANY key to continue...");
+	getch();
 }
 
-void list() {
-	printf("List of Students screen will be shown.\n");
-	return;
+void editStudent() {
+	system("cls");
+	printf("Edit Student Screen\n");
+	printf("Press ANY key to continue...");
+	getch();
+}
+
+void deleteStudent() {
+	system("cls");
+	printf("Delete Student Screen\n");
+	printf("Press ANY key to continue...");
+	getch();
+}
+
+void displayStudent() {
+	system("cls");
+	printf("Display Student Screen\n");
+	printf("Press ANY key to continue...");
+	getch();
+}
+
+void displayAll() {
+	system("cls");
+	printf("Display All Screen\n");
+	printf("Press ANY key to continue...");
+	getch();
+}
+
+void computeGrade() {
+	system("cls");
+	printf("Compute Grade Screen\n");
+	printf("Press ENTER key to continue...");
+	getch();
+}
+
+void generateGSR() {
+	system("cls");
+	printf("Generate Grade Summary Report\n");
+	printf("Press ANY key to continue...");
+	getch();
 }
 
 int main(void) {
-	int option;
-	printf("1) Settings\n");
-	printf("2) List of Students\n");
-	printf("Enter an option: ");
-	scanf("%d", &option);
+	int select;
+	while(1) {
+		system("cls");
+		printf("MAIN MENU\n");
+		printf("1) Add New Students\n");
+		printf("2) Edit Student\n");
+		printf("3) Delete Student\n");
+		printf("4) Display Student\n");
+		printf("5) Display All\n");
+		printf("6) Compute Grade\n");
+		printf("7) Generate Grade Summary Report\n");
+		printf("8) Exit\n");
+		printf("Enter choice (1/2/3/4/5/6/7/8): ");
+		scanf("%d", &select);
 
-	switch(option) {
-		case 1:
-			settings();
-			break;
-		case 2:
-			list();
-			break;
-		default:
-			printf("Invalid option.\n");
+		switch(select) {
+			case 1:
+				addStudent();
+				break;
+			case 2:
+				editStudent();
+				break;
+			case 3:
+				deleteStudent();
+				break;
+			case 4:
+				displayStudent();
+				break;
+			case 5:
+				displayAll();
+				break;
+			case 6:
+				computeGrade();
+				break;
+			case 7:
+				generateGSR();
+				break;
+			case 8:
+				exit(0);
+			default:
+				system("cls");
+		}
 	}
-
-	return 0;
 }
