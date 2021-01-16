489c642045843fd30b651b4800f2f11a1974d495
jovan bruno <jaredivan.bruno@tup.edu.ph>
2021-01-16 14:57:24 +0800

    Project 2 Updates

--- Computer Prog Project 2/source.c
+++ Computer Prog Project 2/source.c
@@ -2,52 +2,92 @@
 #include <stdlib.h>
 #include <conio.h>
 
+FILE *recordptr;
+
 void addStudent() {
 	system("cls");
-	printf("Add Student Screen\n");
-	printf("Press ANY key to continue...");
+
+	char name[50], studentNo[50], choice;
+	float grade1, grade2, grade3, grade4;
+
+	printf("Enter Student No: ");
+	scanf("%s", &studentNo);
+	fflush(stdin);
+
+	printf("Enter Name: ");
+	gets(name);
+	fflush(stdin);
+
+	printf("Enter Score 1: ");
+	scanf("%f", &grade1);
+	fflush(stdin);
+
+	printf("Enter Score 2: ");
+	scanf("%f", &grade2);
+	fflush(stdin);
+
+	printf("Enter Score 3: ");
+	scanf("%f", &grade3);
+	fflush(stdin);
+
+	printf("Enter Score 4: ");
+	scanf("%f", &grade4);
+	fflush(stdin);
+
+	printf("\nSave this record (Y/N)?: ");
+	scanf("%c", &choice);
+	if(choice == 'Y' || choice == 'y') {
+		printf("\tSaving student record...\n");
+		recordptr = fopen("record.txt", "a");
+		fprintf(recordptr, "%s %s %.2f %.2f %.2f %.2f\n", studentNo, name, grade1, grade2, grade3, grade4);
+		fclose(recordptr);
+	} else {
+		printf("\tData discarded.\n");
+	}
+
+	printf("\nPress ANY key to return to MAIN MENU...");
 	getch();
 }
 
 void editStudent() {
 	system("cls");
 	printf("Edit Student Screen\n");
-	printf("Press ANY key to continue...");
+	printf("\nPress ANY key to return to MAIN MENU...");
 	getch();
 }
 
 void deleteStudent() {
 	system("cls");
 	printf("Delete Student Screen\n");
-	printf("Press ANY key to continue...");
+	printf("\nPress ANY key to return to MAIN MENU...");
 	getch();
 }
 
 void displayStudent() {
 	system("cls");
 	printf("Display Student Screen\n");
-	printf("Press ANY key to continue...");
+	printf("\nPress ANY key to return to MAIN MENU...");
 	getch();
 }
 
 void displayAll() {
 	system("cls");
 	printf("Display All Screen\n");
-	printf("Press ANY key to continue...");
+	printf("\nPress ANY key to return to MAIN MENU...");
 	getch();
 }
 
 void computeGrade() {
 	system("cls");
 	printf("Compute Grade Screen\n");
-	printf("Press ENTER key to continue...");
+	printf("\nPress ANY key to return to MAIN MENU...");
 	getch();
 }
 
 void generateGSR() {
 	system("cls");
 	printf("Generate Grade Summary Report\n");
-	printf("Press ANY key to continue...");
+	printf("\nPress ANY key to return to MAIN MENU...");
 	getch();
 }
 
@@ -55,8 +95,8 @@ int main(void) {
 	int select;
 	while(1) {
 		system("cls");
-		printf("MAIN MENU\n");
-		printf("1) Add New Students\n");
+		printf("MAIN MENU\n\n");
+		printf("1) Add New Student\n");
 		printf("2) Edit Student\n");
 		printf("3) Delete Student\n");
 		printf("4) Display Student\n");
@@ -64,7 +104,7 @@ int main(void) {
 		printf("6) Compute Grade\n");
 		printf("7) Generate Grade Summary Report\n");
 		printf("8) Exit\n");
-		printf("Enter choice (1/2/3/4/5/6/7/8): ");
+		printf("\nEnter choice (1/2/3/4/5/6/7/8): ");
 		scanf("%d", &select);
 
 		switch(select) {
